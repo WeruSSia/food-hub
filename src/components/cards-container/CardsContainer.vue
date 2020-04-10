d6f3d3bd6db85a5dbaf73f5937303994b1ff598c
Konrad Adamik <zawias1904@gmail.com>
2020-04-10 21:31:05 +0200

    #13 - Create cards container component

--- src/components/cards-container/CardsContainer.vue
+++ src/components/cards-container/CardsContainer.vue
@@ -1,3 +1,46 @@
 <template>
-    <span> HERE YOU WILL PASS CARDS DATA AND HAVE THEM DISPLAYED </span>
+    <div class="cards-container">
+        <RecipeCard
+            v-for="recipe in recipeList"
+            :data="recipe"
+            :key="recipe.id"
+        />
+    </div>
 </template>
+<script>
+import RecipeCard from "@/components/recipe-card/RecipeCard.vue";
+import { getMockRecipeData } from "../../mock/RecipeData";
+export default {
+    components: {
+        RecipeCard,
+    },
+    data() {
+        return {
+            recipeList: getMockRecipeData(),
+        };
+    },
+};
+</script>
+<style scoped>
+.cards-container {
+    align-content: center;
+    column-gap: 20px;
+    display: grid;
+    grid-template-columns: repeat(3, auto);
+    row-gap: 20px;
+    padding: 0px 20px;
+}
+
+@media screen and (max-width: 650px) {
+    .cards-container {
+        grid-template-columns: repeat(2, auto);
+    }
+}
+
+@media screen and (max-width: 450px) {
+    .cards-container {
+        grid-template-columns: repeat(1, auto);
+        justify-self: center;
+    }
+}
+</style>
