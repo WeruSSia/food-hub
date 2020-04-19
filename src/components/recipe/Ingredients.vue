c75f063308ce00fa97bc0529907782ba87cfde1d
Weronika Mirek <werussia16@gmail.com>
2020-04-19 20:11:01 +0200

    Ingredients section in recipe details page

---
+++ src/components/recipe/Ingredients.vue
@@ -0,0 +1,62 @@
+<template>
+	<div>
+		<div class="header">
+			<span class="header-name">
+				Ingredients
+			</span>
+		</div>
+		<div class="ingredients-list">
+			<div
+				class="checkbox"
+				v-for="ingredient in recipe.extendedIngredients"
+				v-bind:key="ingredient.id"
+			>
+				<span>
+					<input type="checkbox" class="checkbox-input" />
+					<label class="checkbox-label">
+						{{ ingredient.original }}</label
+					>
+				</span>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	props: {
+		recipe: {
+			type: Object,
+			required: true,
+		},
+	},
+};
+</script>
+
+<style scoped>
+.header {
+	border-bottom: 1.5px solid rgb(236, 236, 236);
+	width: 95%;
+	margin: auto;
+	text-align: left;
+}
+.header-name {
+	color: #333333;
+	font-size: 26px;
+	padding: 5px;
+}
+.ingredients-list {
+	text-align: left;
+	padding: 20px 50px 20px 50px;
+	font-size: 14px;
+}
+.checkbox-input {
+	width: 30px;
+	height: 30px;
+	vertical-align: middle;
+}
+
+.checkbox {
+	line-height: 40px;
+}
+</style>
--- src/views/Recipe.vue
+++ src/views/Recipe.vue
@@ -1,16 +1,19 @@
 <template>
 	<div class="recipe">
 		<BasicInfo :recipe="recipe" />
+		<Ingredients :recipe="recipe" />
 	</div>
 </template>
 
 <script>
 import BasicInfo from "@/components/recipe/BasicInfo.vue";
+import Ingredients from "@/components/recipe/Ingredients.vue";
 import { getRecipeById } from "../mock/RecipeData";
 export default {
 	name: "Recipe",
 	components: {
 		BasicInfo,
+		Ingredients,
 	},
 	mounted() {
 		this.recipe = getRecipeById(this.$route.params.id);
