15d9fdbf20dc9703601bfacba6faa192e18d57d7
Arek Hadała <arkadiusz.hadala@wp.pl>
2020-04-23 20:30:31 +0200

    updated API services

--- src/services/services.js
+++ src/services/services.js
@@ -1,8 +1,7 @@
 /**
  * All API calls will be maintainted here
  */
-const API_KEY = "5c4fccbf09894f23b5a22cec1c932533";
-var response = "";
+
 
 /**
  *
@@ -10,70 +9,84 @@ var response = "";
  * @param {number} number - defines # of returned receipes
  * @returns {Array}
  */
-
-export async function getResultByName(query, number, size) {
-    axios
-        .get(
-            "https://api.spoonacular.com/recipes/search?apiKey=" +
-            API_KEY +
-            "&query=" +
-            query +
-            "&number=" +
-            number
-        )
-        .then(response => {
-            this.response = response.results.map(el => {
-                const type = el.image.split('.')[1];
-                el.image = `https://spoonacular.com/recipeImages/${el.id}-${size}.${type}`
-                return el;
-            })
-        });
-    return response;
+export async function getResultByName(query, number) {
+    axios({
+        "method": "GET",
+        "url": "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/search",
+        "headers": {
+            "content-type": "application/octet-stream",
+            "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+            "x-rapidapi-key": "bb78fe07aamshc088cf396e29648p11c6f1jsn311f43158ebd"
+        }, "params": {
+            "intolerances": "egg%2C gluten",
+            "number": number,
+            "offset": "0",
+            "type": "main course",
+            "query": query
+        }
+    })
+        .then((response) => {
+            return response
+        })
+        .catch((error) => {
+            console.log(error)
+        })
 }
 
 /**
  *
 
- * @param {string} ingredients - A comma-separated list of ingredients that the recipes should contain.
+ * @param {Array} of ingredients
  * @returns {Array}
  */
 
 export async function getRecipesByIngredients(ingredients, number) {
-    axios
-        .get(
-            "https://api.spoonacular.com/recipes/findByIngredients?apiKey=" +
-            API_KEY +
-            "&ingredients=" +
-            ingredients +
-            "&number=" +
-            number
-        )
-        .then(response => {
-            this.response = response.data.results;
-        });
-    return response;
-}
+    const ingriedientsString = ingredients.map(ingredient =>
+        ingredient + '%2C'
+    );
 
+    axios({
+        "method": "GET",
+        "url": "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients",
+        "headers": {
+            "content-type": "application/octet-stream",
+            "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+            "x-rapidapi-key": "bb78fe07aamshc088cf396e29648p11c6f1jsn311f43158ebd"
+        }, "params": {
+            "number": number,
+            "ranking": "1",
+            "ingredients": ingriedientsString
+        }
+    })
+        .then((response) => {
+            return response
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+}
 /**
  *
  * @param {string} number - The number of random recipes to be returned (between 1 and 100).
  * @returns {Array} randomly picked recipes
  */
 
-export async function getRandomRecipes(number, size) {
-    axios
-        .get(
-            "https://api.spoonacular.com/recipes/random?apiKey=" +
-            API_KEY +
-            "&number=" +
-            number
-        )
+export async function getRandomRecipes(number) {
+    axios({
+        "method": "GET",
+        "url": "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/random",
+        "headers": {
+            "content-type": "application/octet-stream",
+            "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+            "x-rapidapi-key": "bb78fe07aamshc088cf396e29648p11c6f1jsn311f43158ebd"
+        }, "params": {
+            "number": number
+        }
+    })
         .then(response => {
-            this.response = response.results.map(el => {
-                const type = el.image.split('.')[1];
-                el.image = `https://spoonacular.com/recipeImages/${el.id}-${size}.${type}`
-                return el;
-            })
-        });
-    return response;
+            return response
+        })
+        .catch((error) => {
+            console.log(error)
+        })
 }
