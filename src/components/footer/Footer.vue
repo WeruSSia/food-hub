e17f20b47e0c4ce5a281508a72568b10075969ff
Weronika Mirek <werussia16@gmail.com>
2020-04-29 14:37:33 +0200

    About link added with no href yet

--- src/components/footer/Footer.vue
+++ src/components/footer/Footer.vue
@@ -1,18 +1,30 @@
 <template>
-    <span class="footer">
-        Copyright © 2020 FoodHUB Project Team. All rights reserved
-    </span>
+	<div class="footer">
+		<span class="footer-info">
+			Copyright © 2020 FoodHUB Project Team. All rights reserved.
+			<a href="" class="about">About</a></span
+		>
+	</div>
 </template>
 <style scoped>
 .footer {
-    align-items: center;
-    background-color: rgba(85, 85, 85, 1);
-    bottom: 0;
-    color: #ffffff;
-    display: flex;
-    justify-content: center;
-    height: 80px;
-    position: absolute;
-    width: 100%;
+	align-items: center;
+	background-color: rgba(85, 85, 85, 1);
+	bottom: 0;
+	color: #ffffff;
+	display: flex;
+	justify-content: center;
+	height: 80px;
+	position: absolute;
+	width: 100%;
+}
+.footer-info {
+	margin: 20px;
+}
+.about {
+	color: #ffffff;
+}
+.about:hover {
+	color: #bbbbbb;
 }
 </style>
