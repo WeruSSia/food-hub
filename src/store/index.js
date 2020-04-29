d9142e38c186891d11e14b9aaf130ca86dcef19a
kc3 <kamil.cala@softnet.com.pl>
2020-04-30 00:54:09 +0200

    #117 - added loader while searching recipes

--- src/store/index.js
+++ src/store/index.js
@@ -5,15 +5,11 @@ Vue.use(Vuex);
 export default new Vuex.Store({
 	state: {
 		isUserLoggedIn: false,
-		searchResult: {},
 		searchData: {},
 	},
 	mutations: {
 		setUserLoggedIn(state, payload) {
 			state.isUserLoggedIn = payload;
-		},
-		setSearchResult(state, payload) {
-			state.searchResult = payload;
 		},
 		setSearchData(state, payload) {
 			state.searchData = payload;
