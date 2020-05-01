40fbe8ae3d7285e20e7b813e90cca0a9ca388f05
Konrad Adamik <zawias1904@gmail.com>
2020-05-01 15:14:41 +0200

    #90 - Remove unused filter from profile svg

--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -103,12 +103,11 @@
 					fill="#555555"
 					width="60px"
 					height="60px"
-					filter="url(#f3)"
 				>
+					<path d="M0 0h24v24H0z" fill="none" />
 					<path
 						d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 3c1.66 0 3 1.34 3 3s-1.34 3-3 3-3-1.34-3-3 1.34-3 3-3zm0 14.2c-2.5 0-4.71-1.28-6-3.22.03-1.99 4-3.08 6-3.08 1.99 0 5.97 1.09 6 3.08-1.29 1.94-3.5 3.22-6 3.22z"
 					/>
-					<path d="M0 0h24v24H0z" fill="none" />
 				</svg>
 			</div>
 		</div>
@@ -381,7 +380,7 @@ export default {
 	},
 	data() {
 		return {
-			query: "",
+			query: "v1",
 			keywords: "",
 			include: "",
 			includings: [],
